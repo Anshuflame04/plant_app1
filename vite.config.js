aa46bdd9413f4e6fc2980e533deacfc3f13b4fd6
Anshuflame04 <anshuflame242@gmail.com>
2024-10-23 00:06:46 +0530

    2nd try

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // This allows the app to be accessible on the local network
-    port: 5173, // You can change the port if needed
-    strictPort: true, // Ensures the server fails if the port is already in use
-  },
-  esbuild: {
-    loader: 'jsx', // This tells esbuild to parse .js files as JSX
+    host: true, // Enable network access
+    port: 5173, // Specify port
+    strictPort: true, // Fail if port is in use
   },
   resolve: {
     alias: {
-      'react-native$': 'react-native-web', // Use react-native-web for web
+      // Only needed if you're using react-native-web
+      'react-native$': 'react-native-web',
     },
   },
 });
